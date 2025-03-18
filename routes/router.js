16e8214c0e94620243b08ae506ba8c4ea9b37e5f
Teemu <teemuikonen@live.fi>
2025-03-18 14:09:27 +0200

    added image Adding and deleting

--- routes/router.js
+++ routes/router.js
@@ -6,6 +6,8 @@ const {
   checkUserPw,
   checkUserRole,
 } = require("../services/userManagementService.js");
+const { join } = require("path/posix");
+const { Console } = require("console");
 require("dotenv").config();
 
 const allowedOrigin = process.env.URL;
@@ -62,6 +64,74 @@ router.patch("/ratings", (req, res) => {
         return;
       }
       res.status(200).send("Rating added successfully");
+    });
+  });
+});
+
+// PATCH images to images.json
+router.patch("/images", (req, res) => {
+  const filePath = path.join(__dirname, "../assets/images.json");
+  const newImages = req.body;
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading images.json:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    let oldImages = JSON.parse(data);
+    try {
+      oldImages.images.push(newImages);
+    } catch (error) {
+      console.log("Error patching images ", error);
+    }
+    fs.writeFile(filePath, JSON.stringify(oldImages), "utf8", (err) => {
+      if (err) {
+        console.error("Error writing file", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      res.status(200).send("Images added successfully");
+    });
+  });
+});
+
+// DELETE selected images from images.json
+router.delete("/images", (req, res) => {
+  const filePath = path.join(__dirname, "../assets/images.json");
+  const imagesToDelete = req.body;
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading images.json:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    let oldImages = JSON.parse(data);
+
+    imagesToDelete.sort(function (a, b) {
+      return b - a;
+    });
+    try {
+      for (let i = oldImages.images.length; i >= 0; i--) {
+        for (let j = imagesToDelete.length; j >= 0; j--) {
+          if (i === imagesToDelete[j]) {
+            oldImages.images.splice(i, 1);
+            console.log("Deleted image index: " + i);
+          }
+        }
+      }
+    } catch (error) {
+      Console.log("Error deleting images ", error);
+    }
+
+    fs.writeFile(filePath, JSON.stringify(oldImages), "utf8", (err) => {
+      if (err) {
+        console.error("Error writing file", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+      res.status(200).send("Images deleted successfully");
     });
   });
 });
